2141ecf490a16ec45e2029ef12de08d3e7cbb361
Roger <rgbyford@gmail.com>
2019-01-09 08:44:34 -0600

    deployed

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -2,26 +2,10 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 const styles = {
-  heading: {
-    blackground: "#3f51bl5",
-    minHeight: 50,
-    lineHeight: 3.5,
-    fontSize: "1.2rem",
-    color: "white",
-    padding: "0 20px"
-  },
-  bodyStyle: {
-    backgroundImage: `url(../assets/images/oriental.png)`,
-    margin:-10
-  },
-  content: {
-    padding: 20
-  },
   imgResponsive: {
     width: 200,
     margin: 20
   },
-      // The gray background
   backdropStyle: {
       position: 'fixed',
       top: 0,
@@ -31,7 +15,6 @@ const styles = {
       backgroundColor: 'rgba(0,0,0,0.3)',
       padding: 50
     },
-    // The modal "window"
     modalStyle: {
       backgroundColor: '#FFF',
       borderRadius: 5,
@@ -48,11 +31,8 @@ const styles = {
     }
 };
 
-//  <div style={{display: "flex", justifyContent: "center"}}   >
-//let sNumString1 = "./assets/images/1.jpg";
-//let sNumString3 = "./assets/images/3.jpg";
 let iImages = 12;
-
+let aiArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 let asStrings = [];
 let abAlreadyClicked = [];
 let aiFindClicked = [];
@@ -90,9 +70,7 @@ class Difficulty extends React.Component {
     });
  
   }
-      //<select value="difficulty" onChange={(e) => this.diffChange.bind(this, e.target.value)}></select>
   render() {
-//    console.log ("diffSel");
     return (
       <div>
       <h3>&nbsp;How many do you want to have to click on to win?&nbsp;&nbsp;
@@ -107,9 +85,7 @@ class Difficulty extends React.Component {
 class ClickButton extends React.Component {
   constructor(props) {
     super(props);
-    //    this.state = {sNumString: sNumString1};
     this.state = { isOpen: false, bLose: false };
-
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
   }
@@ -129,24 +105,17 @@ class ClickButton extends React.Component {
     });
   }
 
-
+  
   handleClick(param) {
-    let aiNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let aiNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    console.log("p: ", param);  // the number of the button
-    console.log ("aiFC: ", aiFindClicked);
     let iClicked = aiFindClicked[param];  // picture num
-    console.log("picture num: ", iClicked);
-    console.log("already been clicked: ", abAlreadyClicked);
     iClickedCount = abAlreadyClicked.filter(c => c === true).length + 1;
-    console.log("how many clicked: ", iClickedCount);
     if (abAlreadyClicked[iClicked]) {  // this picture already clicked
-      console.log("Lost!");
       this.toggleModal("You lose!  Boo!");
       iBestScore = Math.max (iBestScore, iClickedCount);
     }
     else if (iClickedCount >= iDiff) {
-      console.log("Won");
       this.toggleModal("You win!  Yay!");
       iBestScore = Math.max (iBestScore, iClickedCount);
     }
@@ -156,71 +125,46 @@ class ClickButton extends React.Component {
     let iNumsLeft = iImages;
     for (let i = 0; i < iImages; i++) {
       iChooser = Math.floor(Math.random() * iNumsLeft);
-//      console.log("random: ", aiNumbers[iChooser]);
       asStrings[i] = `./assets/images/${aiNumbers[iChooser]}.jpg`;
-      //      console.log (asStrings[i]);
       aiFindClicked[i] = aiNumbers[iChooser];
       aiNumbers.splice(iChooser, 1);
-      //      console.log(aiNumbers);
       iNumsLeft--;
-      //      console.log(iChooser);
     }
-    //console.log(this);
     this.setState(() => ({
       //      sNumString: asStrings
     }));
   }
 
-        // <button onClick={this.toggleModal}>
-        //   Open the modal
-        // </button>
-        
-        render() {
-//          console.log("cb render");
-          return (
-            <div>
-              <br/>
-              <h3>&nbsp;Click on the pictures of London.  But don't click on the same one twice!</h3>
-            <div>
-              <Difficulty />
-            </div>
+  render() {
+    return (
       <div>
-      <img className="images" id="pic1" onClick={this.handleClick.bind(this, 0)} alt="" style={styles.imgResponsive} src={asStrings[0]}/>
-      <img className="images" id="pic2" onClick={this.handleClick.bind(this, 1)} alt="" style={styles.imgResponsive} src={asStrings[1]}/>
-      <img className="images" id="pic3" onClick={this.handleClick.bind(this, 2)} alt="" style={styles.imgResponsive} src={asStrings[2]}/>
-      <img className="images" id="pic4" onClick={this.handleClick.bind(this, 3)} alt="" style={styles.imgResponsive} src={asStrings[3]}/>
-      <img className="images" id="pic5" onClick={this.handleClick.bind(this, 4)} alt="" style={styles.imgResponsive} src={asStrings[4]}/>
-      <img className="images" id="pic6" onClick={this.handleClick.bind(this, 5)} alt="" style={styles.imgResponsive} src={asStrings[5]}/>
-      <img className="images" id="pic7" onClick={this.handleClick.bind(this, 6)} alt="" style={styles.imgResponsive} src={asStrings[6]}/>
-      <img className="images" id="pic8" onClick={this.handleClick.bind(this, 7)} alt="" style={styles.imgResponsive} src={asStrings[7]}/>
-      <img className="images" id="pic9" onClick={this.handleClick.bind(this, 8)} alt="" style={styles.imgResponsive} src={asStrings[8]}/>
-      <img className="images" id="pic10" onClick={this.handleClick.bind(this, 9)} alt="" style={styles.imgResponsive} src={asStrings[9]}/>
-      <img className="images" id="pic11" onClick={this.handleClick.bind(this, 10)} alt="" style={styles.imgResponsive} src={asStrings[10]}/>
-      <img className="images" id="pic12" onClick={this.handleClick.bind(this, 11)} alt="" style={styles.imgResponsive} src={asStrings[11]}/>
-      </div>
-      <div>
-
-              <Modal show={this.state.isOpen}>
-              <h3>{this.state.sLoseWin}</h3>
-              <button style={styles.closeButton} onClick={this.toggleModal}>
+        <br />
+        <h3>&nbsp;Click on the pictures of London.  But don't click on the same one twice!</h3>
+        <div>
+          <Difficulty />
+        </div>
+        <div>
+          {aiArray.map((index) =>
+            <img key={index} className="images" id="pic2" onClick={this.handleClick.bind(this, index - 1)} alt="" style={styles.imgResponsive} src={asStrings[index - 1]} />
+          )}
+        </div>
+        <div>
+          <Modal show={this.state.isOpen}>
+            <h3>{this.state.sLoseWin}</h3>
+            <button style={styles.closeButton} onClick={this.toggleModal}>
               Close
             </button>
-
-              </Modal>
-              </div>
-              </div>
+          </Modal>
+        </div>
+      </div>
     );
   }
 }
 
-//   <input type="text" readOnly value={this.state.sLoseWin} />
-
 class Modal extends React.Component {
   render() {
     // Render nothing if the "show" prop is false
-//    console.log ("renderModal");
     if(!this.props.show) {
-//      console.log ("no show");
       return null;
     }
 
@@ -237,21 +181,13 @@ class Modal extends React.Component {
 }
 
 function JSXVariables() {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = { isOpen: false };
-  // }
-
   Modal.propTypes = {
-      onClose: PropTypes.func.isRequired,
       show: PropTypes.bool,
       children: PropTypes.node
   };
 
+  document.body.style.backgroundImage= `url(../assets/images/oriental.png)`;
   return (
-  <body style={styles.bodyStyle}>
-
   <div className="mainContainer">
       <div className="container">
       <div id="outer"></div>
@@ -262,7 +198,6 @@ function JSXVariables() {
       </div>
       </div>
   </div>
-  </body>
   );
 }
 
